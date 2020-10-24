7091cf96d158334b8081edb2ed9163c8937ca60d
qmr <5657985@qq.com>
2020-10-24 16:37:02 +0800

    风犬少年的天空

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>风犬少年的天空</h1>
+    <h1>时光不老，我们不散</h1>
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603532966662&di=8804fc374eae2a73e61d5b9b1b78d5c5&imgtype=0&src=http%3A%2F%2Fp1.itc.cn%2Fimages01%2F20200924%2Ff0f146e4444245d598120069b8e9711c.jpeg">
   </div>
 </template>
