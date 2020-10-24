bcdae18ee96f14d452e10d9897f42c9ee692651c
qmr <5657985@qq.com>
2020-10-24 15:11:06 +0800

    风犬

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <h2>时光不老，我们不散</h2>
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603532966662&di=8804fc374eae2a73e61d5b9b1b78d5c5&imgtype=0&src=http%3A%2F%2Fp1.itc.cn%2Fimages01%2F20200924%2Ff0f146e4444245d598120069b8e9711c.jpeg">
   </div>
 </template>
