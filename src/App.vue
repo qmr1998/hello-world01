f3a1825caeac2c15566d5248cd9265cea527cbce
qmr <5657985@qq.com>
2020-10-24 14:45:38 +0800

    石原里美

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
-    <h2>祝大家前程似锦</h2>
-    <h2>今天也是充满希望的一天啊</h2>
-    <h2>加油</h2>
-    <h2>加油</h2>
-    <h2>加油</h2>
-    <img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4274992844,101101388&fm=26&gp=0.jpg" />
-    <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603469966131&di=f4804ae230c211e6a36958ccafbe76a2&imgtype=0&src=http%3A%2F%2Fwww.07073.com%2Fuploads%2F140717%2F6647734_113629_1.gif" />
-    <img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2798957898,882676378&fm=26&gp=0.jpg" />
+    <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603531972810&di=f0fffd8d634da9a00b18c02d5c02e7c3&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201709%2F03%2F20170903194212_uP4MQ.jpeg">
   </div>
 </template>
 
